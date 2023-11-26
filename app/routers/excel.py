81c4b5809a9e96b91959cc46507a51d796e23633
Chris Auyeung <cpchun@gmail.com>
2023-11-26 22:04:52 +0800

    add background services

--- app/routers/excel.py
+++ app/routers/excel.py
@@ -9,7 +9,8 @@ from concurrent.futures import ThreadPoolExecutor
 from io import BytesIO
 
 import pandas as pd
-from fastapi import APIRouter, Depends, FastAPI, HTTPException
+from fastapi import (APIRouter, BackgroundTasks, Depends, FastAPI,
+                     HTTPException, UploadFile)
 from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
 from starlette.requests import Request
@@ -19,23 +20,20 @@ from app.databases.database import SessionLocal, engine
 
 from ..dependencies import get_token_header
 
-# router = APIRouter(
-#     prefix="/excel",
-#     tags=["process excel"],
-#     dependencies=[Depends(get_token_header)],
-#     responses={404: {"description": "Not found"}},
-# )
-
 router = APIRouter(
     prefix="/excel",
     tags=["process excel"],
 )
 
+background_router = APIRouter(
+    prefix="/background",
+    tags=["background service"],
+)
+
 executor = ThreadPoolExecutor()
 
-
 @router.post('/process_excel')
-async def process_excel(request: Request):
+async def process_excel(request: Request, background_tasks: BackgroundTasks):
     excel_data = await request.body()
 
     df = pd.read_excel(BytesIO(excel_data))
@@ -47,18 +45,12 @@ async def process_excel(request: Request):
     folder_path = f"E:/Dev/Data/Working/{random_num}"
     os.makedirs(folder_path)
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        tasks = []
-        for product_type, group_df in grouped_df:
-            task = executor.submit(perform_data_transformation, group_df, folder_path, product_type, random_num)
-            tasks.append(task)
-
-        # Wait for all tasks to complete
-        concurrent.futures.wait(tasks)
+    for product_type, group_df in grouped_df:
+        # Schedule the data transformation task in the background
+        background_tasks.add_task(perform_data_transformation, group_df, folder_path, product_type, random_num)
 
     # Return the folder path containing all output files
     return folder_path
-
 
 def perform_data_transformation(df, folder_path, product_type, random_num):
     # Perform your actual data transformation logic here
@@ -75,3 +67,37 @@ def perform_data_transformation(df, folder_path, product_type, random_num):
 
     # Simulate some computational delay
     time.sleep(random.randrange(0, 10))
+
+def perform_data_processing(file_path):
+    # Perform your data processing logic here
+    # For example, you can read the Excel file and process its data
+
+    # Load the Excel file into a DataFrame
+    df = pd.read_excel(file_path)
+
+    # Perform your data processing operations on the DataFrame
+    # ...
+
+    # Simulate some computational delay
+    time.sleep(random.randrange(0, 10))
+
+    # Example: Save the processed DataFrame to a new Excel file
+    random_num = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+    output_file_path = f"/path/to/output/{random_num}_processed.xlsx"
+    df.to_excel(output_file_path, index=False)
+
+@router.post('/upload')
+async def upload_file(file: UploadFile, background_tasks: BackgroundTasks):
+    # Generate a unique filename for the uploaded file
+    random_num = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+    file_path = f"/path/to/uploads/{random_num}_{file.filename}"
+
+    # Save the uploaded file to disk
+    with open(file_path, 'wb') as f:
+        f.write(await file.read())
+
+    # Schedule the data processing task in the background
+    background_tasks.add_task(perform_data_processing, file_path)
+
+    # Return a successful response to the VBA client
+    return {'status': 'ok'}
